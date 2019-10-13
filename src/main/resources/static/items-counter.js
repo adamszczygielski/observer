31f44c836dbf257e7b96661b371a6bf0a8897eab
aszczygielski <aszczygielski@gmail.com>
2019-10-13 15:07:30 +0200

    refactor

--- src/main/resources/static/items-counter.js
+++ src/main/resources/static/items-counter.js
@@ -1,7 +1,7 @@
 sumVal = 0;
 var table = document.getElementById("items-table");
 for(var i = 1; i < table.rows.length - 1; i++) {
-    sumVal = sumVal + parseInt(table.rows[i].cells[5].innerText);
+    sumVal = sumVal + parseInt(table.rows[i].cells[4].innerText);
 };
 if(sumVal > 0) {
     document.title = "(" + sumVal + ")" + " " + document.title;
