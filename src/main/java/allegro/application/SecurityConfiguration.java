dfd4fc7871425034bc214820900c70ac2e1b37e3
Adam Szczygielski <aszczygielski@gmail.com>
2020-07-26 18:29:55 +0200

    add custom login page

--- src/main/java/allegro/application/SecurityConfiguration.java
+++ src/main/java/allegro/application/SecurityConfiguration.java
@@ -2,18 +2,22 @@ package allegro.application;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
                 .antMatchers("/h2-console/**").permitAll()
-                .antMatchers("/searches/**").authenticated()
-                .antMatchers("/items/**").authenticated()
-                .and().formLogin().permitAll();
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll().and()
+                .logout().permitAll();
 
         httpSecurity.csrf().disable();
         httpSecurity.headers().frameOptions().disable();
--- src/main/java/allegro/application/api/controller/ItemController.java
+++ src/main/java/allegro/application/api/controller/ItemController.java
@@ -5,9 +5,7 @@ import allegro.application.service.ApplicationService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
