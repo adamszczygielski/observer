56d740e083b8f5806627c313529462871d864800
Adam Szczygielski <aszczygielski@gmail.com>
2020-08-02 18:18:16 +0200

    hide hyperlink if no new items

--- src/main/resources/public/resources/search-counter.js
+++ src/main/resources/public/resources/search-counter.js
@@ -1,8 +1,14 @@
-sum = 0;
+var sum = 0;
+var text;
 var table = document.getElementById("items-table");
+
 for (var i = 1; i < table.rows.length; i++) {
-    sum += parseInt(table.rows[i].cells[5].innerText);
+    text = table.rows[i].cells[5].innerText;
+    if (text) {
+        sum += parseInt(text);
+    }
 }
+
 if (sum > 0) {
     document.title = "(" + sum + ")" + " " + document.title;
 }
