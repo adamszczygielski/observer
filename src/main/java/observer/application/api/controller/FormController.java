3b51fe2ac897f16c9159c338ae6d00805257aee0
Adam Szczygielski <aszczygielski@gmail.com>
2020-08-20 21:47:59 +0200

    refactor FormController

--- src/main/java/observer/application/api/controller/FormController.java
+++ src/main/java/observer/application/api/controller/FormController.java
@@ -1,11 +1,9 @@
 package observer.application.api.controller;
 
-import observer.application.api.SearchDto;
-import observer.application.service.source.allegro.AllegroService;
 import lombok.AllArgsConstructor;
+import observer.application.api.SearchDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,12 +14,9 @@ public class FormController {
 
     public static final String API_PATH = "/form";
 
-    private final AllegroService allegroService;
-
     @RequestMapping(method = RequestMethod.GET)
-    public String form(Model model, @ModelAttribute("searchDto") SearchDto searchDto) {
+    public String form(Model model) {
         model.addAttribute("searchDto", new SearchDto());
-        model.addAttribute("categories", allegroService.getCategories(searchDto.getCategory()));
         return "form";
     }
 }
