f1f1bb25615a2ccbd9c1872db6b02b609f70e088
Adam Szczygielski <aszczygielski@gmail.com>
2020-09-13 20:47:46 +0200

    RedirectController

---
+++ src/main/java/observer/application/api/controller/RedirectController.java
@@ -0,0 +1,18 @@
+package observer.application.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(RedirectController.API_PATH)
+public class RedirectController {
+
+    public static final String API_PATH = "/";
+
+    @GetMapping
+    public ModelAndView redirect() {
+        return new ModelAndView("redirect:" + ItemController.API_PATH);
+    }
+}
